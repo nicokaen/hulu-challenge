22f60f3cb7ed1fbbaf2b35d17d203935a9277e4c
Nicolas Kaen <nicokaen@gmail.com>
2025-04-03 20:57:55 -0700

    Fix some a11y issues

--- src/utils/errors.utils.ts
+++ src/utils/errors.utils.ts
@@ -1,7 +1,9 @@
 export function onError(error: unknown) {
   console.error('Error running the app:', error);
-  
+
   const errorMessage = document.createElement('div');
+  errorMessage.setAttribute('role', 'alert');
+  errorMessage.setAttribute('aria-live', 'assertive');
   errorMessage.textContent =
     'Oops! It seems like we took a wrong turn at Albuquerque. Please try again later.';
   errorMessage.classList.add('error-message');
