8e060d279df3c3e7fb8a0671a32c3c539d9f5188
Nicolas Kaen <nicokaen@gmail.com>
2025-04-03 21:03:08 -0700

    Remove mock data

--- src/api/api.ts
+++ src/api/api.ts
@@ -8,20 +8,14 @@
  * - Collections: https://d1q0vy0v52gyjr.cloudfront.net/collections/<id>.json
  */
 
-import mock from "./mock";
 import { HuluHub, CollectionComponent } from "./types";
 
 export const fetchHuluHub = async (): Promise<HuluHub> => {
-  //   const response = await fetch(
-  //     "https://d1q0vy0v52gyjr.cloudfront.net/hub.json"
-  //   );
-  //   const data = await response.json();
-  //   return data;
-
-  // simulate network delay
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  return Promise.resolve(mock);
+    const response = await fetch(
+      "https://d1q0vy0v52gyjr.cloudfront.net/hub.json"
+    );
+    const data = await response.json();
+    return data;
 };
 
 export const fetchHubById = async (id: string): Promise<CollectionComponent> => {
